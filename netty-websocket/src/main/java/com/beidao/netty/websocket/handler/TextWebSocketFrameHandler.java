3c4d2c0c486c796df2da30f4acc35df5f3ae45e4
beidao <MAXAmbitious@163.com>
2018-09-13 11:43:44 +0800

    111
    11111

---
+++ netty-websocket/src/main/java/com/beidao/netty/websocket/handler/TextWebSocketFrameHandler.java
@@ -0,0 +1,31 @@
+package com.beidao.netty.websocket.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+	private final ChannelGroup group;
+
+	public TextWebSocketFrameHandler(ChannelGroup group) {
+		this.group = group;
+	}
+
+	@Override
+	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+		if (evt == WebSocketServerProtocolHandler.ServerHandshakeStateEvent.HANDSHAKE_COMPLETE) {
+			ctx.pipeline().remove(HttpRequestHandler.class);
+			group.writeAndFlush(new TextWebSocketFrame("Client " + ctx.channel() + " joined"));
+			group.add(ctx.channel());
+		} else {
+			super.userEventTriggered(ctx, evt);
+		}
+	}
+
+	@Override
+	public void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+		group.writeAndFlush(msg.retain());
+	}
+}
